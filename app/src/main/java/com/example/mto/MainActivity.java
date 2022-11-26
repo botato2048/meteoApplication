09461a1fe4e81036b82508e28ce9d037ab4b3b8a
botato2048 <ema@tfs.ca>
2022-11-26 10:14:52 -0500

    Initial commit

---
+++ app/src/main/java/com/example/mto/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.mto;
+
+import static com.android.volley.Request.*;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class MainActivity extends AppCompatActivity {
+    //Déclaration des champs
+    TextView date,city, temperature, Description;
+    ImageView imageView5;
+    String maVille="Toronto";
+    private RequestQueue requestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        date=findViewById(R.id.date);
+        city=findViewById(R.id.city);
+        temperature=findViewById(R.id.temperature);
+        Description=findViewById(R.id.Description);
+        requestQueue= new Volley.requestQueue(this);
+        requestQueue.add(jsonObjectRequest);
+        afficher();
+
+    }
+public void afficher() {
+    String url = "https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=74ba4b0d6163b7741666f4e8d4d845fa&units=metric";
+    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Method.GET, url, new Response.Listener<JSONObject>() {
+        public void onResponse(JSONObject response) {
+            try {
+                JSONObject main_object = response.getJSONObject("main");
+                JSONArray array = response.getJSONArray("weather");
+                Log.d("tag","resultat = "+ array.toString());
+                System.out.print("salut");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }, new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+
+        }
+
+    });
+}
+}
